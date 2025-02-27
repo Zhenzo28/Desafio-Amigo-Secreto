dfa4fb338037c041eb67aa145c1ed8c9cb6d2bb4
Sergio Gallego <sgallegobastidas03@gmail.com>
2025-02-27 11:51:49 -0500

    En el app.js se crearon las funciones de agregarAmigo y ActualizarLista

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo(){
+    // Capturar el input de la variable llamada nombre
+    let nombre = document.getElementById('amigo').value;
+    // Validar vacio
+    if(nombre.trim() === ''){
+        alert('Por favor, inserte un nombre');
+        return;
+    }
+    // añadir el nombre al array amigos
+    amigos.push(nombre);
+    // Limpiar el input
+    document.getElementById('amigo').value = '';
+}
+
+function actualizarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
